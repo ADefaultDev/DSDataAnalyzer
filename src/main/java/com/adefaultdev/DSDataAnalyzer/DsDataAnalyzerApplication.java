5f58e95e6bc3c48516f111a3cd15506793f500de
ADefaultDev <im7ckngmad@gmail.com>
2025-07-07 20:43:55 +0500

    Initial commit

---
+++ src/main/java/com/adefaultdev/DSDataAnalyzer/DsDataAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.adefaultdev.DSDataAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsDataAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsDataAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adefaultdev/DSDataAnalyzer/DsDataAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adefaultdev.DSDataAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsDataAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
